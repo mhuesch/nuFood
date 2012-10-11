0d7f2ec3c64ffdda6c8034b92c5794c445101a8e
Michael Hueschen <michaelhueschen@gmail.com>
2012-10-11 12:05:09 -0500

    Clean up.

--- nuFood/urls.py
+++ nuFood/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-#from nuFood import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -16,8 +15,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-
-    #url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root':settings.STATIC_ROOT, 'show_indexes':False}),
 )
 
 urlpatterns += staticfiles_urlpatterns()
