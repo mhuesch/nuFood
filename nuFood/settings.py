e98ec045b43076d87cc1fe69e86c975360d7fefd
Michael Hueschen <michaelhueschen@gmail.com>
2012-10-11 12:02:01 -0500

    Now using gunicorn webserver, static directory to serve admin content. Admin enabled.

--- nuFood/settings.py
+++ nuFood/settings.py
@@ -70,6 +70,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    '/static/',
 )
 
 # List of finder classes that know how to find static files in
@@ -118,11 +119,11 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gunicorn',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'gunicorn',
 )
 
 # A sample logging configuration. The only tangible logging
--- nuFood/urls.py
+++ nuFood/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+#from nuFood import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -14,4 +16,8 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    #url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root':settings.STATIC_ROOT, 'show_indexes':False}),
 )
+
+urlpatterns += staticfiles_urlpatterns()
