6d3ebbc82a0d2b106abf1f647d4da004e456d1ba
Andrew Howes <AndrewHowes2013@u.northwestern.edu>
2012-10-14 15:41:29 -0500

    Added basic list of halls + function for current time

--- halls/views.py
+++ halls/views.py
@@ -1,4 +1,15 @@
+from django.template import Context, loader
 from django.http import HttpResponse
+from halls.models import Hall
+from datetime import datetime
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the nuFood index.")
+    halls = Hall.objects.all()
+    d = datetime.now()
+    t = loader.get_template('index.html')
+    c = Context({
+                'Halls': halls,
+                'date': d
+                })
+    return HttpResponse(t.render(c))
+#return HttpResponse("Hello, world. You're at the nuFood index.")
