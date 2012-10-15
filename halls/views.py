033f669a5d60b7a113626b7def013214c0efcdc4
Lizz B <huzzah.its.lizz@gmail.com>
2012-10-14 22:13:02 -0500

    filtering to only open now on this day works!

--- halls/views.py
+++ halls/views.py
@@ -12,7 +12,8 @@ def index(request):
     now_minute = d.minute
     #Open = Hour.objects.filter(day=Day).filter(end_hour__gt=now_hour).filter(start_hour__lt=now_hour) | Hour.objects.filter(day=Day).filter(end_hour=now_hour).filter(end_minute__gt=now_minute)
     
-    Open = Hour.objects.filter(Q(end_hour__gt=now_hour) | (Q(end_hour=now_hour) & Q(end_minute__gt=now_minute))).filter(Q(start_hour__lt=now_hour) | (Q(start_hour=now_hour) & Q(start_minute__lt=now_minute)))
+    Open = Hour.objects.filter(Q(end_hour__gt=now_hour) | (Q(end_hour=now_hour) & Q(end_minute__gt=now_minute))).filter(Q(start_hour__lt=now_hour) | (Q(start_hour=now_hour) & Q(start_minute__lt=now_minute))).filter(day=Day)
+    
     
     #.extra(select={(start_hour*60+start_minute) > (now_hour*60+now_minute)}.distinct('host_hall')
 
