d6a354cb7f13a63117f8fa4f99ead54db37cc57e
Lizz B <huzzah.its.lizz@gmail.com>
2012-10-17 13:45:25 -0500

    can now see open and closed dining halls, also 12hr time format
    - 12 hour time format added with am/pm (slight formatting, words don't
    perfectly line up if only single digit hour… but whatever for now)
    
    - added a temporary red or green background color (behind the buttons)
    to show open or closed halls
    
    - still working with andrew to figure out how to not show repeats of
    open halls in the closed halls...

--- halls/views.py
+++ halls/views.py
@@ -17,26 +17,24 @@ def index(request):
                 Q(start_hour__lt=now_hour) | (Q(start_hour=now_hour) & Q(start_minute__lt=now_minute))).filter(
                 day=Day)
     
-    #lizz's attempt to try to see closed places...
-    #Closed = Hour.objects.filter(Q(end_hour__lt=now_hour) | (Q(end_hour=now_hour) & Q(end_minute__lt=now_minute))).filter(Q(start_hour__gt=now_hour) & Q(start_minute__gt=now_minute)).filter(day=Day)
-    ##This won't return anything, I don't think. You would be filtering down to time blocks that have already ended, then filtering those to blocks that haven't started yet. This would mean the end time is earlier than the start time for anything that's returned.
     
     
     ##This query block will return the next time that a hall opens on a day.
     #---------
-    ##filter for blocks of time that haven't ended yet
-    #Closed = Hour.objects.filter(Q(end_hour__gt=now_hour) | (Q(end_hour=now_hour) & Q(end_minute__gt=now_minute)))
-    ##filter for blocks of time that also haven't started yet
-    #Closed = Closed.exclude(Q(start_hour__lt=now_hour) | (Q(start_hour=now_hour) & Q(start_minute__lt=now_minute))).filter(day=Day)
+    #filter for blocks of time that haven't ended yet
+    Closed = Hour.objects.filter(Q(end_hour__gt=now_hour) | (Q(end_hour=now_hour) & Q(end_minute__gt=now_minute)))
+    #filter for blocks of time that also haven't started yet
+    Closed = Closed.exclude(Q(start_hour__lt=now_hour) | (Q(start_hour=now_hour) & Q(start_minute__lt=now_minute))).filter(day=Day)
     ##order by when it starts
-    #Closed = Closed.order_by('host_hall','start_hour','start_minute')
-    ##choose the first time for each hall
-    #Closed = Closed.distinct('host_hall')
+    Closed = Closed.order_by('host_hall','start_hour','start_minute')
+    #choose the first time for each hall
+    Closed = Closed.distinct('host_hall')
+   
+    # exclude halls that are currently open (not working yet)
+    Closed = exclude(Open)
     #---------
     
-    #Closed = Hall.objects.all()
-    
-    #.extra(select={(start_hour*60+start_minute) > (now_hour*60+now_minute)}.distinct('host_hall')
+
 
     
     t = loader.get_template('index.html')
@@ -44,8 +42,9 @@ def index(request):
                 'Halls': halls,
                 'date': d,
                 'open': Open,
-                # 'closed': Closed,
+                'closed': Closed,
                 'day': Day,
                 })
     return HttpResponse(t.render(c))
+
 #return HttpResponse("Hello, world. You're at the nuFood index.")
