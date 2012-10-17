d57b2a6a59aacdb0b597f6c420346ceda17965ee
Andrew Howes <AndrewHowes2013@u.northwestern.edu>
2012-10-17 00:08:24 -0500

    Added a query to show the next time a hall opens on the current day.
    It's currently commented out, but I've commented my logic for
    filtering. If you look at it around midnight it will show the first
    time each of the halls opens for the day (I tested it that way).

--- halls/views.py
+++ halls/views.py
@@ -14,10 +14,28 @@ def index(request):
     
     
     
-    Open = Hour.objects.filter(Q(end_hour__gt=now_hour) | (Q(end_hour=now_hour) & Q(end_minute__gt=now_minute))).filter(Q(start_hour__lt=now_hour) | (Q(start_hour=now_hour) & Q(start_minute__lt=now_minute))).filter(day=Day)
+    Open = Hour.objects.filter(
+                Q(end_hour__gt=now_hour) | (Q(end_hour=now_hour) & Q(end_minute__gt=now_minute))).filter(
+                Q(start_hour__lt=now_hour) | (Q(start_hour=now_hour) & Q(start_minute__lt=now_minute))).filter(
+                day=Day)
     
     #lizz's attempt to try to see closed places...
     #Closed = Hour.objects.filter(Q(end_hour__lt=now_hour) | (Q(end_hour=now_hour) & Q(end_minute__lt=now_minute))).filter(Q(start_hour__gt=now_hour) & Q(start_minute__gt=now_minute)).filter(day=Day)
+    ##This won't return anything, I don't think. You would be filtering down to time blocks that have already ended, then filtering those to blocks that haven't started yet. This would mean the end time is earlier than the start time for anything that's returned.
+    
+    
+    ##This query block will return the next time that a hall opens on a day.
+    #---------
+    ##filter for blocks of time that haven't ended yet
+    #Closed = Hour.objects.filter(Q(end_hour__gt=now_hour) | (Q(end_hour=now_hour) & Q(end_minute__gt=now_minute)))
+    ##filter for blocks of time that also haven't started yet
+    #Closed = Closed.exclude(Q(start_hour__lt=now_hour) | (Q(start_hour=now_hour) & Q(start_minute__lt=now_minute))).filter(day=Day)
+    ##order by when it starts
+    #Closed = Closed.order_by('host_hall','start_hour','start_minute')
+    ##choose the first time for each hall
+    #Closed = Closed.distinct('host_hall')
+    #---------
+    
     #Closed = Hall.objects.all()
     
     #.extra(select={(start_hour*60+start_minute) > (now_hour*60+now_minute)}.distinct('host_hall')
