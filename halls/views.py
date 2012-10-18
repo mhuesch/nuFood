3dca214c34c78900aa7d39bd542616d74795cb77
Michael Hueschen <michaelhueschen@gmail.com>
2012-10-18 14:46:42 -0500

    Added functionality to select hour objects from the day before and merge them into the current day's list of hour objects

--- halls/views.py
+++ halls/views.py
@@ -4,21 +4,50 @@ from django.db.models import Q
 from halls.models import Hall, Hour
 from datetime import datetime
 
+
 def index(request):
     d = datetime.now()
-    Day = d.weekday()
+    weekday = d.weekday()
     now_hour = d.hour       # put test hour here    example test times: 745, 1249
     now_minute = d.minute   # put test minute here
 
     halls = Hall.objects.all()
     
+    # Select hour objects from halls previous day which might still be open
+    now_hour_for_yesterday = now_hour + 24
+    weekday_for_yesterday = (weekday - 1) % 7
+    openFromYesterday = Hour.objects.filter(
+        day=weekday_for_yesterday
+    ).filter(
+        Q(end_hour__gt=now_hour_for_yesterday) | (Q(end_hour=now_hour_for_yesterday) & Q(end_minute__gt=now_minute))
+    ).filter(
+        Q(start_hour__lt=now_hour_for_yesterday) | (Q(start_hour=now_hour_for_yesterday) & Q(start_minute__lt=now_minute))
+    )
+
+    # Select hour objects from current day which are open
     Open = Hour.objects.filter(
-        day=Day
+        day=weekday
     ).filter(
         Q(end_hour__gt=now_hour) | (Q(end_hour=now_hour) & Q(end_minute__gt=now_minute))
     ).filter(
         Q(start_hour__lt=now_hour) | (Q(start_hour=now_hour) & Q(start_minute__lt=now_minute))
     )
+
+    # Function which removes an item from a list if a given predicate evaluates to True
+    def remove_if(coll, predicate):
+        idx = len(coll) - 1
+        while idx >= 0:
+            if predicate(coll[idx]):
+                del coll[idx]
+            idx = idx - 1
+        return coll
+
+    # Use remove_if to remove open hour objects from the Open list if there is an hour object from
+    # yesterday for the same hall.
+    for yesterHour in openFromYesterday:
+        hall_id = yesterHour.host_hall_id
+        Open = [yesterHour] + remove_if(Open, lambda h: (h.host_hall_id == hall_id))
+
     
     # values list returns list of tuples of selected values
     # flat makes it a list instead of tuples
@@ -32,7 +61,7 @@ def index(request):
     ).exclude(
         Q(start_hour__lt=now_hour) | (Q(start_hour=now_hour) & Q(start_minute__lt=now_minute))
     ).filter(
-        day=Day
+        day=weekday
     ).order_by(
         'host_hall','start_hour','start_minute'
     ).distinct(
@@ -50,7 +79,7 @@ def index(request):
                 'date': d,
                 'openHalls': Open,
                 'closedHalls': Closed,
-                'day': Day,
+                'day': weekday,
                 'nowMin': now_minute,
                 'nowHr': now_hour,
                 })
