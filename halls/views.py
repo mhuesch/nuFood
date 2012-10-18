2570f86acbae381f8ed0aff3fa0d1618f5d44c10
Michael Hueschen <michaelhueschen@gmail.com>
2012-10-18 13:43:09 -0500

    Make queries more readable

--- halls/views.py
+++ halls/views.py
@@ -12,7 +12,13 @@ def index(request):
 
     halls = Hall.objects.all()
     
-    Open = Hour.objects.filter(day=Day).filter(Q(end_hour__gt=now_hour) | (Q(end_hour=now_hour) & Q(end_minute__gt=now_minute))).filter(Q(start_hour__lt=now_hour) | (Q(start_hour=now_hour) & Q(start_minute__lt=now_minute)))
+    Open = Hour.objects.filter(
+        day=Day
+    ).filter(
+        Q(end_hour__gt=now_hour) | (Q(end_hour=now_hour) & Q(end_minute__gt=now_minute))
+    ).filter(
+        Q(start_hour__lt=now_hour) | (Q(start_hour=now_hour) & Q(start_minute__lt=now_minute))
+    )
     
     # values list returns list of tuples of selected values
     # flat makes it a list instead of tuples
@@ -21,17 +27,19 @@ def index(request):
     ##This query block will return the next time that a hall opens on a day.
     #---------
     #filter for blocks of time that haven't ended yet
-    Closed = Hour.objects.filter(Q(end_hour__gt=now_hour) | (Q(end_hour=now_hour) & Q(end_minute__gt=now_minute)))
-    #filter for blocks of time that also haven't started yet
-    Closed = Closed.exclude(Q(start_hour__lt=now_hour) | (Q(start_hour=now_hour) & Q(start_minute__lt=now_minute))).filter(day=Day)
-    ##order by when it starts
-    Closed = Closed.order_by('host_hall','start_hour','start_minute')
-    #choose the first time for each hall
-    Closed = Closed.distinct('host_hall')
-   
-    # exclude halls that are currently open 
-    Closed = Closed.exclude(host_hall__in=list(open_halls))
-    #---------
+    Closed = Hour.objects.filter(
+        Q(end_hour__gt=now_hour) | (Q(end_hour=now_hour) & Q(end_minute__gt=now_minute))
+    ).exclude(
+        Q(start_hour__lt=now_hour) | (Q(start_hour=now_hour) & Q(start_minute__lt=now_minute))
+    ).filter(
+        day=Day
+    ).order_by(
+        'host_hall','start_hour','start_minute'
+    ).distinct(
+        'host_hall'
+    ).exclude(
+        host_hall__in=list(open_halls)
+    )
     
 
 
