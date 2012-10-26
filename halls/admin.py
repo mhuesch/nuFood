421245ee3e39bcaf00d7dc07a8ab6a973e669aca
Michael Hueschen <michaelhueschen@gmail.com>
2012-10-26 18:19:17 -0500

    Don't import removed Category model. Don't register with admin.

--- halls/admin.py
+++ halls/admin.py
@@ -1,9 +1,8 @@
-from halls.models import Hall, Hour, MealMenu, FoodItem, FoodCategory, FoodAttribute
+from halls.models import Hall, Hour, MealMenu, FoodItem, FoodAttribute
 from django.contrib import admin
 
 admin.site.register(Hall)
 admin.site.register(Hour)
 admin.site.register(MealMenu)
 admin.site.register(FoodItem)
-admin.site.register(FoodCategory)
 admin.site.register(FoodAttribute)
